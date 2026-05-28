6c7ee89cf8f9587d82271ca9326377cabea9e12f
Theadora <txh973068733@126.com>
2026-05-28 15:51:23 +0800

    delete key

--- Agent-Code/TravelPlanAgent_v0.1.py
+++ Agent-Code/TravelPlanAgent_v0.1.py
@@ -11,7 +11,7 @@ class LLMConfig:
     集中保存模型调用需要的配置。：base_url、api_key、model_name。
     """
     base_url: str = "https://chat.ecnu.edu.cn/open/api/v1"
-    api_key: str = "sk-4b905783f8ab4fed9f7c1879aaf2ae58"
+    api_key: str = ""
     model_name: str = "ecnu-max"
     temperature: float = 0.7
     timeout_seconds: int = 60
